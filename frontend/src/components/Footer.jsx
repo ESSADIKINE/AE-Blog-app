e0639d4771d576609484317313c289a6e59ee7f9
[200~essadikine.fst@uhp.ac.ma <essadikine.fst@uhp.ac.ma>
2024-06-23 14:27:20 +0100

    update the home page

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Box, Grid, Link, Typography } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import blogLogoGif from '../utils/Logo.gif';
+
+const Footer = ({ categories, portfolio, gmail, linkedin, github }) => {
+  return (
+    <Box component="footer" sx={{ py: 3, px: 30, mt: 'auto', backgroundColor: 'background.paper' }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={3}>
+          <RouterLink to={"/"}>
+            <img src={blogLogoGif} alt="Blog Logo" style={{ width: '160px', marginBottom: '10px' }} />
+          </RouterLink>
+          <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }} style={{ width: '160px', marginBottom: '10px' }}>
+            <Grid item component="a" href="https://portfolio.com" rel="noopener noreferrer" target="_blank">
+              <img alt="portfolio logo" src={portfolio} style={{ width: '24px', height: '24px', marginRight: '10px' }} />
+            </Grid>
+            <Grid item component="a" href="https://www.gmail.com" rel="noopener noreferrer" target="_blank">
+              <img alt="gmail logo" src={gmail} style={{ width: '24px', height: '24px', marginRight: '10px' }} />
+            </Grid>
+            <Grid item component="a" href="https://www.linkedin.com" rel="noopener noreferrer" target="_blank">
+              <img alt="linkedin logo" src={linkedin} style={{ width: '24px', height: '24px', marginRight: '10px' }} />
+            </Grid>
+            <Grid item component="a" href="https://www.github.com" rel="noopener noreferrer" target="_blank">
+              <img alt="github logo" src={github} style={{ width: '24px', height: '24px', marginRight: '10px' }} />
+            </Grid>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={3}>
+          {categories.slice(0, 3).map((section) => (
+            <Link
+              color="inherit"
+              noWrap
+              key={section.name}
+              variant="body2"
+              href={`search?category=${section.urlName}`}
+              sx={{ display: 'block', mb: 1 }}
+            >
+              {section.name}
+            </Link>
+          ))}
+        </Grid>
+        <Grid item xs={12} md={3}>
+          {categories.slice(3, 6).map((section) => (
+            <Link
+              color="inherit"
+              noWrap
+              key={section.name}
+              variant="body2"
+              href={`search?category=${section.urlName}`}
+              sx={{ display: 'block', mb: 1 }}
+            >
+              {section.name}
+            </Link>
+          ))}
+        </Grid>
+        <Grid item xs={12} md={3}>
+          {categories.slice(6, 9).map((section) => (
+            <Link
+              color="inherit"
+              noWrap
+              key={section.name}
+              variant="body2"
+              href={`search?category=${section.urlName}`}
+              sx={{ display: 'block', mb: 1 }}
+            >
+              {section.name}
+            </Link>
+          ))}
+        </Grid>
+      </Grid>
+      <Box sx={{ mt: 4, textAlign: 'center' }}>
+        <Typography variant="body2" color="textSecondary">
+          &copy; {new Date().getFullYear()} <span style={{ color: '#009975' }}>Anass Essadikine Blog</span>. All rights reserved.
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default Footer;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,9 @@
+import React from 'react';
 import { Box, CircularProgress, Container, Grid, Paper, Typography, Toolbar, Link } from "@mui/material";
 import { useGetPopularPostsQuery, useGetRecentPostsQuery } from "../redux/posts/postsApi";
 import PostCard from "../components/PostCard";
-import { Link as RouterLink } from "react-router-dom";
 import { categories } from "../utils/constants";
-import blogLogoGif from '../utils/Logo.gif';
+import Footer from "../components/Footer";
 
 const HomePage = () => {
   // get recent posts
@@ -159,76 +159,13 @@ const HomePage = () => {
           </Typography>
         )}
       </Container>
-      <Box component="footer" sx={{ py: 3, px: 30, mt: 'auto', backgroundColor: 'background.paper' }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={3}>
-            <RouterLink to={"/"}>
-              <img src={blogLogoGif} alt="Blog Logo" style={{ width: '160px', marginBottom: '10px' }} />
-            </RouterLink>
-            <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }} style={{ width: '160px', marginBottom: '10px' }}>
-              <Grid item component="a" href="https://portfolio.com" rel="noopener noreferrer" target="_blank">
-                <img alt="portfolio logo" src={portfolio} style={{ width: '24px', height: '24px', marginRight: '10px' }} />
-              </Grid>
-              <Grid item component="a" href="https://www.gmail.com" rel="noopener noreferrer" target="_blank">
-                <img alt="gmail logo" src={gmail} style={{ width: '24px', height: '24px', marginRight: '10px' }} />
-              </Grid>
-              <Grid item component="a" href="https://www.linkedin.com" rel="noopener noreferrer" target="_blank">
-                <img alt="linkedin logo" src={linkedin} style={{ width: '24px', height: '24px', marginRight: '10px' }} />
-              </Grid>
-              <Grid item component="a" href="https://www.github.com" rel="noopener noreferrer" target="_blank">
-                <img alt="github logo" src={github} style={{ width: '24px', height: '24px', marginRight: '10px' }} />
-              </Grid>
-            </Box>
-          </Grid>
-          <Grid item xs={12} md={3}>
-            {categories.slice(0, 3).map((section) => (
-              <Link
-                color="inherit"
-                noWrap
-                key={section.name}
-                variant="body2"
-                href={`search?category=${section.urlName}`}
-                sx={{ display: 'block', mb: 1 }}
-              >
-                {section.name}
-              </Link>
-            ))}
-          </Grid>
-          <Grid item xs={12} md={3}>
-            {categories.slice(3, 6).map((section) => (
-              <Link
-                color="inherit"
-                noWrap
-                key={section.name}
-                variant="body2"
-                href={`search?category=${section.urlName}`}
-                sx={{ display: 'block', mb: 1 }}
-              >
-                {section.name}
-              </Link>
-            ))}
-          </Grid>
-          <Grid item xs={12} md={3}>
-            {categories.slice(6, 9).map((section) => (
-              <Link
-                color="inherit"
-                noWrap
-                key={section.name}
-                variant="body2"
-                href={`search?category=${section.urlName}`}
-                sx={{ display: 'block', mb: 1 }}
-              >
-                {section.name}
-              </Link>
-            ))}
-          </Grid>
-        </Grid>
-        <Box sx={{ mt: 4, textAlign: 'center' }}>
-          <Typography variant="body2" color="textSecondary">
-            &copy; {new Date().getFullYear()} <span style={{ color: '#1976d2' }}>Anass Essadikine Blog</span>. All rights reserved.
-          </Typography>
-        </Box>
-      </Box>
+      <Footer
+        categories={categories}
+        portfolio={portfolio}
+        gmail={gmail}
+        linkedin={linkedin}
+        github={github}
+      />
     </>
   );
 };
